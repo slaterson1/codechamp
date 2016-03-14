8982691c5b8b75e378415177492a096b591a5126
slatersan1 <cdlee212@gmail.com>
2016-03-14 02:34:20 -0400

    working on looping

--- lib/codechamp/github.rb
+++ lib/codechamp/github.rb
@@ -1,19 +1,29 @@
-require "codechamp/version"
+module CodeChamp
+  class Github
+    include HTTParty
+    base_uri "https://api.github.com"
+    @username = nil
 
-module Codechamp
-	class Github
-		include HTTParty
-		base_uri "https://api.github.com"
-
-		def initialize
-      	@headers = {
-        "Authorization" => "token #{ENV["OAUTH_TOKEN_GH"]}",
+    def initialize(oauth)
+      @headers = {
+        "Authorization" => "token #{oauth}",
         "User-Agent"    => "HTTParty"
-      	}
+      }
     end
 
-    def list_contributions(username, repo)
-      Github.get("repos/#{username}/#{repo}/stats/contributors", headers: @headers)
+    def get_contributions(org, repo)
+      results = []
+      Github.get("/repos/#{org}/#{repo}/stats/contributors", headers: @headers)
+    end
+      
+    def get_data(weeks)
+      contrib_type = "a"
+      contrib_num = 1
+
+      relevant = weeks.select {|h|h.key?(contrib_type) && h[contrib_type]==contrib_num}
+      return nil if relevant.empty?  
+      relevant.each_with_object({}) {|h,g| g.merge!(h) {|k,gv,hv| k == contrib_type ? gv : # is enumerator appropriate?
+                      (gv.to_i + hv.to_i).to_s}}
     end
   end
 end
